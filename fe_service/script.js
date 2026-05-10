5031e65b7c303c5297753eed2b5957a10ea97272
shofiaqilah <aqilahshofi5@gmail.com>
2026-05-10 21:08:58 +0700

    perbarui url

--- fe_service/script.js
+++ fe_service/script.js
@@ -1,4 +1,4 @@
-const API = "https://notes-shofi-dot-a-46-491023.uc.r.appspot.com/";
+const API = "https://be-rest-521926891872.us-central1.run.app/";
 
 async function getNotes() {
   const res = await fetch(API);
